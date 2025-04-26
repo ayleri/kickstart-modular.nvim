9c991cc7f12ad3afca557aea09856d761855ff9e
ayleri <ailenmeng10@gmail.com>
2025-04-26 19:04:40 -0400

    rose-pine

---
+++ lua/custom/plugins/themes/rose-pine.lua
@@ -0,0 +1,18 @@
+-- lua/custom/plugins/themes/rose-pine.lua
+return {
+    "rose-pine/neovim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("rose-pine").setup({
+        variant = "moon", -- "main", "moon", "dawn"
+        styles = {
+          bold = true,
+          italic = false,
+          transparency = false,
+        },
+      })
+      vim.cmd.colorscheme("rose-pine")
+    end,
+  }
+  
