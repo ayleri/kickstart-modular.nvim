294032f013386a000ce0ae010c9ce35fc9276842
ayleri <ailenmeng10@gmail.com>
2025-05-06 22:05:29 -0400

    noice nvim

---
+++ lua/custom/plugins/noice.lua
@@ -0,0 +1,10 @@
+return {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {},
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "rcarriga/nvim-notify"
+    }
+  }
