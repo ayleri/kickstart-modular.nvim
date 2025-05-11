b73e0ffee878717147dbbafed731e5074f71a6d6
ayleri <ailenmeng10@gmail.com>
2025-05-10 20:54:13 -0400

    vim-dispatch, vim-test & toggleterm

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -1,7 +1,29 @@
 return {
     "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {},
+    event = "CmdlineEnter",
+    opts = {
+        cmdline = {
+            format = {
+                cmdline = { title = "" },
+                search_down = { view = "cmdline" }, -- search in command line
+                search_up = { view = "cmdline" },
+                filter = { title = "" },
+                lua = { title = "" },
+                help = { title = "" },
+                input = { title = "" },
+            },
+        },
+        -- hide search virtual text
+        routes = {
+            {
+              filter = {
+                event = "msg_show",
+                kind = "search_count",
+              },
+              opts = { skip = true },
+            },
+          },
+    },
     dependencies = {
       "MunifTanjim/nui.nvim",
       "nvim-lua/plenary.nvim",
---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,16 @@
+return {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("toggleterm").setup {
+        size = 20,
+        open_mapping = [[<c-\>]], -- is this better in keymaps or no ??
+        hide_numbers = true,
+        shade_terminals = true,
+        start_in_insert = true,
+        direction = "float",  -- or "horizontal"/"vertical"
+        float_opts = {
+          border = "curved",
+        },
+      }
+    end
+  }
