ed7acfe3e426ce1af668a1fb0f68feb9727b683c
ayleri <ailenmeng10@gmail.com>
2025-05-14 10:18:52 -0400

    replaced more : with <cmd>

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -72,22 +72,22 @@ vim.keymap.set('n', 'n', 'nzzzv', opts)
 vim.keymap.set('n', 'N', 'Nzzzv', opts)
 
 -- Resize with arrows
-vim.keymap.set('n', '<Up>', ':resize -2<CR>', opts)
-vim.keymap.set('n', '<Down>', ':resize +2<CR>', opts)
-vim.keymap.set('n', '<Left>', ':vertical resize -2<CR>', opts)
-vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
+vim.keymap.set('n', '<Up>', '<cmd>resize -2<CR>', opts)
+vim.keymap.set('n', '<Down>', '<cmd>resize +2<CR>', opts)
+vim.keymap.set('n', '<Left>', '<cmd>vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<Right>', '<cmd>vertical resize +2<CR>', opts)
 
 -- Buffers
-vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)          -- next buffer
-vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)    -- prev buffer
-vim.keymap.set('n', '<leader>x', ':bdelete!<CR>', opts)   -- close buffer
+vim.keymap.set('n', '<Tab>', '<cmd>bnext<CR>', opts)          -- next buffer
+vim.keymap.set('n', '<S-Tab>', '<cmd>bprevious<CR>', opts)    -- prev buffer
+vim.keymap.set('n', '<leader>x', '<cmd>bdelete!<CR>', opts)   -- close buffer
 vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
 
 -- Window management
 vim.keymap.set('n', '<leader>v', '<C-w>v', opts)      -- split window vertically
 vim.keymap.set('n', '<leader>h', '<C-w>s', opts)      -- split window horizontally
 vim.keymap.set('n', '<leader>se', '<C-w>=', opts)     -- make split windows equal width & height
-vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split window
+vim.keymap.set('n', '<leader>xs', '<cmd>close<CR>', opts) -- close current split window
 
 -- -- Tabs
 -- vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
